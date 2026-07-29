bcaee25d99d51b80d88f76c915ab76f1f4c94084
Laurent Yee <yeeqixuan1@gmail.com>
2026-07-29 03:46:21 +0100

    updated vite

--- config/vite.config.ts
+++ config/vite.config.ts
@@ -8,6 +8,7 @@ import react from '@vitejs/plugin-react'
 const configDir = fileURLToPath(new URL('.', import.meta.url))
 
 export default defineConfig({
+  base: '/PretextTool/',
   plugins: [react()],
   css: {
     postcss: configDir
